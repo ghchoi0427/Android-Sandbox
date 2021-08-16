c4655fe17067856f099e3f2b599beef4e20bf6ce
ghchoi0427 <ghchoi0427@gmail.com>
2021-08-16 22:55:24 +0900

    feat: RealtimeDBActivity 생성

--- app/src/main/java/com/example/firebase210724/MainActivity.java
+++ app/src/main/java/com/example/firebase210724/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.firebase210724;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonStorage;
+    Button buttonDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonStorage = findViewById(R.id.btn_storage);
+        buttonDatabase = findViewById(R.id.btn_database);
+
+        buttonStorage.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), StorageActivity.class)));
+        buttonDatabase.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), RealtimeDBActivity.class)));
     }
 }
