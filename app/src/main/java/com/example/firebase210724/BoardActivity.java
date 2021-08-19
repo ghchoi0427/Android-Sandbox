4b3071396bf4f22f94d4b279b3fd8bac9c3486aa
ghchoi0427 <ghchoi0427@gmail.com>
2021-08-19 19:58:15 +0900

    feat: BoardActivity 생성

---
+++ app/src/main/java/com/example/firebase210724/BoardActivity.java
@@ -0,0 +1,16 @@
+package com.example.firebase210724;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board);
+
+
+    }
+}
