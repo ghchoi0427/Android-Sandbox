497032d326bbcbdb5bf7a380e97031b5f850775c
ghchoi0427 <ghchoi0427@gmail.com>
2021-08-20 00:44:16 +0900

    feat: 글쓰기 인텐트 연결

--- app/src/main/java/com/example/firebase210724/BoardActivity.java
+++ app/src/main/java/com/example/firebase210724/BoardActivity.java
@@ -1,16 +1,23 @@
 package com.example.firebase210724;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BoardActivity extends AppCompatActivity {
+    Button btnBoardUpload;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
 
+        btnBoardUpload = findViewById(R.id.btn_board_upload);
+
+        btnBoardUpload.setOnClickListener(view -> startActivity(new Intent(BoardActivity.this, UploadActivity.class)));
 
     }
 }
