c2fa3bcaa2c27739c63d04cac95884104dc878c5
ghchoi0427 <ghchoi0427@gmail.com>
2021-08-20 08:57:11 +0900

    feat: 실행 시 자동으로 불러오기 기능

--- app/src/main/java/com/example/firebase210724/BoardActivity.java
+++ app/src/main/java/com/example/firebase210724/BoardActivity.java
@@ -5,9 +5,18 @@ import android.os.Bundle;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.firebase210724.adapter.BoardAdapter;
+import com.example.firebase210724.util.FirebaseDatabaseHandler;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class BoardActivity extends AppCompatActivity {
     Button btnBoardUpload;
+    RecyclerView recyclerBoard;
+    BoardAdapter boardAdapter;
+    FirebaseDatabaseHandler handler;
 
     @Override
 
@@ -15,9 +24,24 @@ public class BoardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
 
+        boardAdapter = new BoardAdapter();
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        recyclerBoard = findViewById(R.id.recycler_board);
+        recyclerBoard.setLayoutManager(layoutManager);
+        recyclerBoard.setAdapter(boardAdapter);
+
+        handler = new FirebaseDatabaseHandler(FirebaseFirestore.getInstance());
+
         btnBoardUpload = findViewById(R.id.btn_board_upload);
+
+        loadBoard();
 
         btnBoardUpload.setOnClickListener(view -> startActivity(new Intent(BoardActivity.this, UploadActivity.class)));
 
+    }
+
+    private void loadBoard() {
+        handler.setBoardList(getApplicationContext(), boardAdapter);
     }
 }
