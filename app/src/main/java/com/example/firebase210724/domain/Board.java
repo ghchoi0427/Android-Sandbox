f5766ff60af28ffeedde19cca59c9e838a8335c8
ghchoi0427 <ghchoi0427@gmail.com>
2021-08-20 07:41:41 +0900

    fix: 잘못된 import 변경

--- app/src/main/java/com/example/firebase210724/domain/Board.java
+++ app/src/main/java/com/example/firebase210724/domain/Board.java
@@ -1,13 +1,18 @@
 package com.example.firebase210724.domain;
 
+
+import com.google.firebase.Timestamp;
+
 public class Board {
     String userId;
     String title;
     String content;
+    Timestamp date;
 
-    public Board(String userId, String content, String title) {
+    public Board(String userId, String title, String content, Timestamp date) {
         this.userId = userId;
-        this.content = content;
         this.title = title;
+        this.content = content;
+        this.date = date;
     }
 }
