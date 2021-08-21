7127cab63c6e94afd2c77f5904a6bb0f98c18df1
ghchoi0427 <ghchoi0427@gmail.com>
2021-08-21 21:16:50 +0900

    fix: 게시판 시간순 배열

--- app/src/main/java/com/example/firebase210724/util/FirebaseDatabaseHandler.java
+++ app/src/main/java/com/example/firebase210724/util/FirebaseDatabaseHandler.java
@@ -5,10 +5,11 @@ import android.widget.Toast;
 
 import com.example.firebase210724.adapter.BoardAdapter;
 import com.example.firebase210724.domain.Board;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -39,26 +40,23 @@ public class FirebaseDatabaseHandler {
 
     public void post(Context context, String title, String content, String userId) {
         Map<String, Object> board = new HashMap<>();
+        DocumentReference documentReference = db.collection(COLLECTION_BOARD).document();
+
         board.put("content", content);
         board.put("title", title);
         board.put("date", new Timestamp(new Date()));
         board.put("userId", userId);
+        board.put("key", documentReference.getId());
 
-
-        db.collection(COLLECTION_BOARD)
-                .add(board)
-                .addOnSuccessListener(documentReference -> Toast.makeText(context, "성공적으로 게시되었습니다.", Toast.LENGTH_SHORT).show()).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(Exception e) {
-                Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show();
-            }
-        });
+        documentReference.set(board)
+                .addOnSuccessListener(unused -> Toast.makeText(context, "성공적으로 게시되었습니다.", Toast.LENGTH_SHORT).show())
+                .addOnFailureListener(e -> Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show());
     }
 
     public void setBoardList(Context context, BoardAdapter boardAdapter) {
         List<Board> boardList = new ArrayList<>();
 
-        db.collection("boards").get().addOnSuccessListener(queryDocumentSnapshots -> {
+        db.collection(COLLECTION_BOARD).orderBy("date", Query.Direction.DESCENDING).get().addOnSuccessListener(queryDocumentSnapshots -> {
             for (DocumentSnapshot document : queryDocumentSnapshots.getDocuments()) {
                 String userId = Objects.requireNonNull(document.getData().get("userId")).toString();
                 String title = Objects.requireNonNull(document.getData().get("title")).toString();
