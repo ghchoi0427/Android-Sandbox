94281f9bd01f159eab462484ec16e5115b92456a
ghchoi0427 <ghchoi0427@gmail.com>
2021-08-22 17:41:50 +0900

    feat: 스케줄러 액티비티 인텐트

--- app/src/main/java/com/example/firebase210724/MainActivity.java
+++ app/src/main/java/com/example/firebase210724/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button btnBoard;
+    Button btnSchedule;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +17,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnBoard = findViewById(R.id.btn_board);
+        btnSchedule = findViewById(R.id.btn_schedule);
 
         btnBoard.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), BoardActivity.class)));
+        btnSchedule.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), ScheduleActivity.class)));
     }
 }
