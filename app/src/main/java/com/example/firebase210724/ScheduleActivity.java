7bba558e93c7e7b3997a358fc637867dba820448
ghchoi0427 <ghchoi0427@gmail.com>
2021-08-23 20:26:55 +0900

    feat: 캘린더뷰 추가

--- app/src/main/java/com/example/firebase210724/ScheduleActivity.java
+++ app/src/main/java/com/example/firebase210724/ScheduleActivity.java
@@ -1,14 +1,19 @@
 package com.example.firebase210724;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ScheduleActivity extends AppCompatActivity {
+    CalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+
+        calendarView = findViewById(R.id.calendarView);
+
     }
 }
