2f927193dc05534cadfd8808d40903a1eaf1b86b
ghchoi0427 <ghchoi0427@gmail.com>
2021-08-24 15:42:13 +0900

    fix: Hashmap이 아닌 Board 클래스 사용하도록 변경

--- app/src/main/java/com/example/firebase210724/util/FirebaseDatabaseHandler.java
+++ app/src/main/java/com/example/firebase210724/util/FirebaseDatabaseHandler.java
@@ -6,16 +6,14 @@ import android.widget.Toast;
 
 import com.example.firebase210724.adapter.BoardAdapter;
 import com.example.firebase210724.domain.Board;
+import com.example.firebase210724.domain.User;
 import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 
 public class FirebaseDatabaseHandler {
@@ -23,29 +21,22 @@ public class FirebaseDatabaseHandler {
     private final FirebaseFirestore db;
     private final String COLLECTION_USER = "users";
     private final String COLLECTION_BOARD = "boards";
+    private final String COLLECTION_SCHEDULE = "schedules";
 
 
     public FirebaseDatabaseHandler(FirebaseFirestore db) {
         this.db = db;
     }
 
-    public void addUser(Context context, String name, String ID) {
-        Map<String, Object> user = new HashMap<>();
-        user.put("name", name);
+    public void addUser(Context context, String name) {
+        User user = new User(name);
 
-        db.collection(COLLECTION_USER).document(ID).set(user)
+        db.collection(COLLECTION_USER).document(user.getName()).set(user)
                 .addOnSuccessListener(unused -> Toast.makeText(context, "user added", Toast.LENGTH_SHORT).show())
                 .addOnFailureListener(e -> Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show());
     }
 
-    public void post(Context context, String title, String content, String userId) {
-        Map<String, Object> board = new HashMap<>();
-
-        board.put("content", content);
-        board.put("title", title);
-        board.put("date", new Timestamp(new Date()));
-        board.put("userId", userId);
-
+    public void postBoard(Context context, Board board) {
         db.collection(COLLECTION_BOARD).document().set(board)
                 .addOnSuccessListener(unused -> Toast.makeText(context, "성공적으로 게시되었습니다.", Toast.LENGTH_SHORT).show())
                 .addOnFailureListener(e -> Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show());
@@ -56,17 +47,18 @@ public class FirebaseDatabaseHandler {
 
         db.collection(COLLECTION_BOARD).orderBy("date", Query.Direction.DESCENDING).get().addOnSuccessListener(queryDocumentSnapshots -> {
             for (DocumentSnapshot document : queryDocumentSnapshots.getDocuments()) {
-                String userId = Objects.requireNonNull(document.getData().get("userId")).toString();
-                String title = Objects.requireNonNull(document.getData().get("title")).toString();
-                String content = Objects.requireNonNull(document.getData().get("content")).toString();
-                Timestamp date = (Timestamp) document.getData().get("date");
-                boardList.add(new Board(userId, title, content, date));
+                Board board = new Board();
+                board.setUserId(Objects.requireNonNull(document.getData().get("userId")).toString());
+                board.setTitle(Objects.requireNonNull(document.getData().get("title")).toString());
+                board.setContent(Objects.requireNonNull(document.getData().get("content")).toString());
+                board.setDate((Timestamp) document.getData().get("date"));
+
+                boardList.add(board);
             }
         }).addOnCompleteListener(task -> {
             boardAdapter.addBoards(boardList);
             boardAdapter.notifyDataSetChanged();
         }).addOnFailureListener(e -> Toast.makeText(context, "게시판 조회 실패 : " + e.getMessage(), Toast.LENGTH_SHORT).show());
-
     }
 
     public void getUserNameById(String id, TextView textView) {
@@ -82,6 +74,10 @@ public class FirebaseDatabaseHandler {
                         textView.setText("unknown user");
                     }
                 });
+    }
+
+    public void setSchedule() {
+        //db.collection()
     }
 
 }
