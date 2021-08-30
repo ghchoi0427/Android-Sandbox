945a848d1412fd79ef92fe871f294617f5a61834
ghchoi0427 <ghchoi0427@gmail.com>
2021-08-30 21:57:27 +0900

    feat: web 버튼 추가

--- app/src/main/java/com/example/firebase210724/MainActivity.java
+++ app/src/main/java/com/example/firebase210724/MainActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.webkit.WebView;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnBoard;
     Button btnSchedule;
+    Button btnWeb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +20,10 @@ public class MainActivity extends AppCompatActivity {
 
         btnBoard = findViewById(R.id.btn_board);
         btnSchedule = findViewById(R.id.btn_schedule);
+        btnWeb = findViewById(R.id.btn_web);
 
         btnBoard.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), BoardActivity.class)));
         btnSchedule.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), ScheduleActivity.class)));
+        btnWeb.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), WebActivity.class)));
     }
 }
