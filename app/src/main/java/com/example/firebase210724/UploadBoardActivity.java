5e0b2533f71d83e7c3623db74d4c7484c98a5854
ghchoi0427 <ghchoi0427@gmail.com>
2021-09-03 21:49:14 +0900

    refactor: renamed from activity_upload

--- app/src/main/java/com/example/firebase210724/UploadBoardActivity.java
+++ app/src/main/java/com/example/firebase210724/UploadBoardActivity.java
@@ -25,7 +25,7 @@ public class UploadBoardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_upload);
+        setContentView(R.layout.activity_upload_board);
 
         editTitle = findViewById(R.id.edit_upload_title);
         editContent = findViewById(R.id.edit_upload_content);
