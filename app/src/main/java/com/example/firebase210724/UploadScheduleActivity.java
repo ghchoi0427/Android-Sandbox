c04857c3df9882b13c074f03fd3937ed39dc1a4e
ghchoi0427 <ghchoi0427@gmail.com>
2021-09-04 21:06:00 +0900

    feat: UploadScheduleActivity 생성

---
+++ app/src/main/java/com/example/firebase210724/UploadScheduleActivity.java
@@ -0,0 +1,13 @@
+package com.example.firebase210724;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UploadScheduleActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+    }
+}
