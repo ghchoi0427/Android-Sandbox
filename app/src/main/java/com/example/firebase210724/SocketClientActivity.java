7d6d277b57b25edc44ad1fe553533d3473971dbc
ghchoi0427 <ghchoi0427@gmail.com>
2021-09-07 22:06:14 +0900

    feat: 소켓 클라이언트 액티비티

---
+++ app/src/main/java/com/example/firebase210724/SocketClientActivity.java
@@ -0,0 +1,12 @@
+package com.example.firebase210724;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SocketClientActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_socket_client);
+    }
+}
